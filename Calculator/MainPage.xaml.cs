ba315b11c099acbc3ba843b909c8ee9a08c7696b
Lewis Houlihan <G00344955@gmit.ie>
2017-12-17 00:32:17 +0000

    Initial commit

---
+++ Calculator/MainPage.xaml.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Calculator
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            result.Text = 0.ToString();
+
+        }
+        private void AddNumberToResult(double number)
+        {
+            if (char.IsNumber(result.Text.Last()))
+            {
+                if (result.Text.Length == 1 && result.Text == "0")
+                {
+                    result.Text = string.Empty;
+                }
+                result.Text += number;
+            }
+            else
+            {
+                if (number != 0)
+                {
+                    result.Text += number;
+                }
+            }
+        } // end of AddNumberToResult
+
+
+        enum Operation {
+            MINUS = 1,
+            PLUS = 2,
+            DIV = 3,
+            TIMES = 4,
+            NUMBER = 5
+        }
+
+        private void AddOperationToResult(Operation operation)
+        {
+            if (result.Text.Length == 1 && result.Text == "0") return;
+
+            if (!char.IsNumber(result.Text.Last()))
+            {
+                result.Text = result.Text.Substring(0, result.Text.Length - 1);
+            }
+
+            switch (operation)
+            {
+                case Operation.MINUS: result.Text += "-"; break;
+                case Operation.PLUS: result.Text += "+"; break;
+                case Operation.DIV: result.Text += "/"; break;
+                case Operation.TIMES: result.Text += "x"; break;
+            }
+        } //end of AddOperationToResult
+
+        #region Equal
+        private class Operand
+        {
+            public Operation operation = Operation.NUMBER; // default
+            public double value = 0;
+
+            public Operand left = null;
+            public Operand right = null;
+        }
+
+        private Operand BuildTreeOperand()
+        {
+            Operand tree = null;
+
+            string expression = result.Text;
+            if (!char.IsNumber(expression.Last()))
+            {
+                expression = expression.Substring(0, expression.Length - 1);
+            }
+
+            string numberStr = string.Empty;
+            foreach (char c in expression.ToCharArray())
+            {
+                if (char.IsNumber(c) || c == '.' || numberStr == string.Empty && c == '-')
+                {
+                    numberStr += c;
+                }
+                else
+                {
+                    AddOperandToTree(ref tree, new Operand() { value = double.Parse(numberStr) });
+                    numberStr = string.Empty;
+
+                    Operation op = Operation.MINUS; // default
+                    switch (c)
+                    {
+                        case '-': op = Operation.MINUS; break;
+                        case '+': op = Operation.PLUS; break;
+                        case '/': op = Operation.DIV; break;
+                        case 'x': op = Operation.TIMES; break;
+                    }
+                    AddOperandToTree(ref tree, new Operand() { operation = op });
+                }
+            }
+            // Last number
+            AddOperandToTree(ref tree, new Operand() { value = double.Parse(numberStr) });
+
+            return tree;
+        }
+
+        private void AddOperandToTree(ref Operand tree, Operand elem)
+        {
+            if (tree == null)
+            {
+                tree = elem;
+            }
+            else
+            {
+                if (elem.operation < tree.operation)
+                {
+                    Operand auxTree = tree;
+                    tree = elem;
+                    elem.left = auxTree;
+                }
+                else
+                {
+                    AddOperandToTree(ref tree.right, elem); // recursive
+                }
+            }
+        }
+
+        private double Calc(Operand tree)
+        {
+            if (tree.left == null && tree.right == null) // it's a number!
+            {
+                return tree.value;
+            }
+            else // it's an operation (-, +, /, x)
+            {
+                double subResult = 0;
+                switch (tree.operation)
+                {
+                    case Operation.MINUS: subResult = Calc(tree.left) - Calc(tree.right); break; // recursive
+                    case Operation.PLUS: subResult = Calc(tree.left) + Calc(tree.right); break;
+                    case Operation.DIV: subResult = Calc(tree.left) / Calc(tree.right); break;
+                    case Operation.TIMES: subResult = Calc(tree.left) * Calc(tree.right); break;
+                }
+                return subResult;
+            }
+        }
+
+
+        #endregion Equal
+
+        private void button7_Click(object sender, RoutedEventArgs e)
+        {
+            AddNumberToResult(7);
+        }
+
+        private void button8_Click(object sender, RoutedEventArgs e)
+        {
+            AddNumberToResult(8);
+        }
+
+        private void button9_Click(object sender, RoutedEventArgs e)
+        {
+            AddNumberToResult(9);
+        }
+
+        private void buttonDivide_Click(object sender, RoutedEventArgs e)
+        {
+            AddOperationToResult(Operation.DIV);
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            AddNumberToResult(4);
+        }
+
+        private void button5_Click(object sender, RoutedEventArgs e)
+        {
+            AddNumberToResult(5);
+        }
+
+        private void button6_Click(object sender, RoutedEventArgs e)
+        {
+            AddNumberToResult(6);
+        }
+
+        private void buttonMultiply_Click(object sender, RoutedEventArgs e)
+        {
+            AddOperationToResult(Operation.TIMES);
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            AddNumberToResult(1);
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            AddNumberToResult(2);
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            AddNumberToResult(3);
+        }
+
+        private void buttonMinus_Click(object sender, RoutedEventArgs e)
+        {
+            AddOperationToResult(Operation.MINUS);
+        }
+
+        private void buttonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            result.Text = 0.ToString();
+        }
+
+        private void button0_Click(object sender, RoutedEventArgs e)
+        {
+            AddNumberToResult(0);
+        }
+
+        private void buttonEquals_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(result.Text)) return;
+
+            Operand tree = BuildTreeOperand(); 
+
+            double value = Calc(tree); 
+
+            result.Text = value.ToString();
+        }
+
+        private void buttonPlus_Click(object sender, RoutedEventArgs e)
+        {
+            AddOperationToResult(Operation.PLUS);
+        }
+    }
+}
